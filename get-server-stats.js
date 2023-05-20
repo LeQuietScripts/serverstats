b53bf0eaa96eeb536725321fbadfb96c248b965e
LeQuietScripts <134173125+LeQuietScripts@users.noreply.github.com>
2023-05-20 15:54:38 -0600

    Create get-server-stats.js

---
+++ get-server-stats.js
@@ -0,0 +1,34 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+	var target = ns.args[0];
+  // Provides stats of targeted server, including the name, max money, min security, used & max ram, security level, hacking chance, & root access
+	const hostName = ns.args[0];
+	var moneyThresh = ns.getServerMaxMoney(target);
+	var securityThresh = ns.getServerMinSecurityLevel(target);
+	let availableRAM = ns.getServerMaxRam(target);
+	let usedRAM = ns.getServerUsedRam(target);
+	const securityLevel = ns.getServerSecurityLevel(target);
+	var chance = ns.hackAnalyzeChance(target);
+	const root = ns.hasRootAccess(target);
+
+	// Prints all stats from above into terminal.
+	ns.tprint("Server Statistics");
+	ns.tprint("Host Name: ", hostName);
+	ns.tprint("Max Money: ", ns.formatNumber(moneyThresh));
+	ns.tprint("Min Security: ", securityThresh);
+	ns.tprint("Available Ram: ", usedRAM, "GB of ", availableRAM, "GB");
+	ns.tprint("Security Level: ", securityLevel);
+	ns.tprint("Hacking Chance: ", chance);
+	ns.tprint("Has Root Access: ", root);
+
+	// Checks which ports are open.
+	const ssh = ns.getServer(target).sshPortOpen;
+	const http = ns.getServer(target).httpPortOpen;
+	const smtp = ns.getServer(target).smtpPortOpen;
+	const sql = ns.getServer(target).sqlPortOpen;
+	const ftp = ns.getServer(target).ftpPortOpen;
+	var requiredPorts = ns.getServerNumPortsRequired(target);
+	ns.tprint("SSH: ", ssh, " HTTP: ", http, " SMTP: ", smtp, " SQL: ", sql, " FTP: ", ftp);
+	ns.tprint("Required Ports: ", requiredPorts);
+}
